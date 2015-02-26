869ba8ec3c22266b936da3088b2617aa8ae0d43b
ashic <ashic@live.com>
2015-02-26 14:57:40 +0000

    first drop

---
+++ manifests/site.pp
@@ -0,0 +1,24 @@
+
+node 'win-2008r2-std' {
+     quicksilver::web_baseline { 'qsweb':
+         msdeploy_path => 'c:\vagrant\WebDeploy_amd64_en-US.msi'
+     }
+    ->
+    iis::manage_app_pool {'MyWebAppForDeploymentAppPool':
+      enable_32_bit           => true,
+      managed_runtime_version => 'v4.0',
+    }
+    ->
+    iis::manage_site {'MyWebAppForDeployment':
+      site_path     => 'C:\inetpub\wwwroot\MyWebAppForDeployment',
+      port          => '8057',
+      ip_address    => '*',
+      #host_header   => 'www.mysite.com',
+      app_pool      => 'MyWebAppForDeploymentAppPool'
+    }->
+    quicksilver::qs_website{ 'MyWebAppForDeployment':
+        environment => "qa",
+        publishroot => "c:\\vagrant",    
+        version => "v0.11"
+    }
+}
